fcc95ef2a3bbc0aa629843c18adc92ac46d72d56
Jen Ihl <jenniferlindatruong@gmail.com>
2021-10-28 02:43:44 +0000

    looping through objects that has arrays

---
+++ eqObjects.js
@@ -0,0 +1,40 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion failed: ${actual} === ${expected}`);
+  }
+}
+
+/*First, use the Object.keys function on both objects and compare their lengths. If not equal, there's no point in going further: return false
+After that check, loop through the keys returned by Object.keys for one of the objects (not both). Use for..of since the keys are an array. Other loop types can work too, but this one is most elegant and idiomatic, thanks to ES6
+Inside our loop, compare both objects' values for that key. Use === to ensure that the types are the same!
+As soon as there is not a match, we can return false
+Our control flow will therefore only get to the end of the loop if all the keys matched. We can and should thus return true at the end (after the loop)*/ 
+const eqObjects = function(object1, object2) {
+  let keys1 = Object.keys(object1);
+  let keys2 = Object.keys(object2);
+  let result = false;
+  if (keys1.length === keys2.length) {
+    keys1.forEach(element => {
+      if (keys2.includes(element) && object1[element] === object2[element]) 
+      {
+        result = true; 
+      } else if (Array.isArray(object1[element]) && Array.isArray(object2[element])) {
+        result = eqArrays(object1[element], object2[element]);
+      } else {
+        result = false;
+      }
+    });
+  }
+  return result;
+};
+
+    
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba)); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc))
